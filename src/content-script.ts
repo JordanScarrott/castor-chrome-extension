1e223fb9af6a18a08d24a421c0be500dc6e53a96
Jordan Scarrott <jordan.scarrott@vastech.co.za>
2025-10-12 15:07:07 +0200

    Resolved merge conflict

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,14 +1,4 @@
-import { Readability } from "@mozilla/readability";
-import { ElementSelector } from "./element-selector";
-
-const article = new Readability(document.cloneNode(true)).parse();
-
-if (article) {
-    chrome.runtime.sendMessage({
-        type: "parsed-article",
-        payload: article,
-    });
-}
+import { ElementSelector } from "@/element-selector";
 
 chrome.runtime.onMessage.addListener((message) => {
     if (message.type === "ACTIVATE_SELECTION_MODE") {
