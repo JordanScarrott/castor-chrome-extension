ddb8b91269a5d4227722800e0eaf1371259957b7
Jordan Scarrott <jordan.scarrott@vastech.co.za>
2025-10-16 21:19:34 +0200

    Fixed styling

--- src/popup/views/NewSessionView.vue
+++ src/popup/views/NewSessionView.vue
@@ -1,7 +1,7 @@
 <template>
-  <WelcomeScreen />
+    <WelcomeScreen style="height: 500px" />
 </template>
 
 <script setup lang="ts">
-import WelcomeScreen from '../components/WelcomeScreen.vue';
+import WelcomeScreen from "../components/WelcomeScreen.vue";
 </script>
