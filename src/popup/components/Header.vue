1b7fd6c9fdd61710ddb1abfc032534e32c06e2d1
Jordan Scarrott <jordan.scarrott@vastech.co.za>
2025-10-18 21:08:11 +0200

    Small style improvements

--- src/popup/components/Header.vue
+++ src/popup/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="app-header" role="banner">
         <CastorIcon class="header-icon" @click="handleIconClick" />
-        <h1 class="header-title">Castor</h1>
+        <h1 class="header-title" @click="handleIconClick">Castor</h1>
     </header>
 </template>
 
@@ -28,8 +28,8 @@ const handleIconClick = () => {
 }
 
 .header-icon {
-    width: 24px;
-    height: 24px;
+    width: 28px;
+    height: 28px;
     margin-right: 0.5rem;
     cursor: pointer;
 }
@@ -39,5 +39,6 @@ const handleIconClick = () => {
     font-weight: 600;
     color: #1f2937;
     margin: 0;
+    cursor: pointer;
 }
 </style>
